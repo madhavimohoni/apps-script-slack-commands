afccedfad001737bbe39bea765637a71d1e639ad
Madhavi Mohoni <madhavimohoni@gmail.com>
2020-01-21 17:38:10 +0530

    Literally a semicolon

--- index.js
+++ index.js
@@ -16,7 +16,7 @@
     4. Add your new command on api.slack.com/apps with the request URL as the current web app URL from step 4.
 
   *****************************************************************************************************************/       
-const myCommand = require('./commands/myCommand')
+const myCommand = require('./commands/myCommand');
 /**
  *   Inbuilt POST request endpoint function in Google Apps Script. Must be named doPost
  */
